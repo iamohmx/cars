223874be22618e4849c22d3e31bed4d9de6e18af
iamohmx_ <ohmzdev.contact@gmail.com>
2024-09-21 09:17:25 +0700

    first commit

---
+++ src/main/java/com/cars/Main.java
@@ -0,0 +1,7 @@
+package com.cars;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/cars/config/DBConnection.java
@@ -0,0 +1,5 @@
+package com.cars.config;
+
+public class DBConnection {
+    
+}
