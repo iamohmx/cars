34d96932336cd26b3de9d0942d9f0a3c5791924f
iamohmx_ <ohmzdev.contact@gmail.com>
2024-09-21 10:07:11 +0700

    ConnectDB!!!! '🌟🚀

---
+++ src/main/java/com/cars/config/DBConfig.java
@@ -0,0 +1,10 @@
+package com.cars.config;
+
+public class DBConfig {
+    String url, username, password;
+    DBConfig (String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+}
